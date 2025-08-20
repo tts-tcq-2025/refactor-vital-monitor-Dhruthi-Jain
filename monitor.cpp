c5732a8e1fa6d191f189a982a52fcc3c7f80bfbe
Dhruthi-Jain <83956221+Dhruthi-Jain@users.noreply.github.com>
2025-08-20 16:46:10 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -2,16 +2,19 @@
 #include "./thresholds.h"
 #include "./alerts.h"
 
-bool vitalsOk(float temperature, float pulseRate, float spo2) {
+bool vitalsOk(float temperature, float pulseRate, float spo2) 
+{
     if (!isTemperatureOk(temperature)) {
         showAlert("Temperature is critical!");
         return false;
     }
-    if (!isPulseOk(pulseRate)) {
+    if (!isPulseOk(pulseRate)) 
+    {
         showAlert("Pulse Rate is out of range!");
         return false;
     }
-    if (!isSpO2Ok(spo2)) {
+    if (!isSpO2Ok(spo2)) 
+    {
         showAlert("Oxygen Saturation is out of range!");
         return false;
     }
