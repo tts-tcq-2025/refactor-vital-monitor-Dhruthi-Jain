eb1ed6c01ae045feb2a99edf6dfdeaa96581b8a5
Dhruthi-Jain <83956221+Dhruthi-Jain@users.noreply.github.com>
2025-08-20 16:08:45 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,35 +1,6 @@
 #include "./monitor.h"
-#include <assert.h>
-#include <thread>
-#include <chrono>
-#include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-
-bool isTemperatureOk(float temperature) {
-    return (temperature >= 95 && temperature <= 102);
-}
-
-bool isPulseOk(float pulseRate) {
-    return (pulseRate >= 60 && pulseRate <= 100);
-}
-
-bool isSpO2Ok(float spo2) {
-    return (spo2 >= 90);
-}
-
-void blinkWarning() {
-    for (int i = 0; i < 6; ++i) {
-        cout << "\r* " << flush;
-        sleep_for(seconds(1));
-        cout << "\r *" << flush;
-        sleep_for(seconds(1));
-    }
-}
-
-void showAlert(const char* message) {
-    cout << message << '\n';
-    blinkWarning();
-}
+#include "./thresholds.h"
+#include "./alerts.h"
 
 bool vitalsOk(float temperature, float pulseRate, float spo2) {
     if (!isTemperatureOk(temperature)) {
@@ -41,7 +12,7 @@ bool vitalsOk(float temperature, float pulseRate, float spo2) {
         return false;
     }
     if (!isSpO2Ok(spo2)) {
-        showAlert("Oxygen Saturation out of range!");
+        showAlert("Oxygen Saturation is out of range!");
         return false;
     }
     return true;
